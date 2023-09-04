93d387a1dd319daaf7d3e91758e0f23b9acebf53
rmagur1203 <rmagur1203@naver.com>
2023-09-04 10:14:48 +0900

    fix: optionsOA is not defined

--- src/Barcode.svelte
+++ src/Barcode.svelte
@@ -1,25 +1,25 @@
 <script>
-  import { onMount, tick, afterUpdate } from 'svelte';
-  import JsBarcode from 'jsbarcode';
+  import { onMount, tick, afterUpdate } from "svelte";
+  import JsBarcode from "jsbarcode";
 
   let barcode;
   export let value;
-  export let elementTag = 'img';
+  export let elementTag = "img";
   export let options;
   const defaultOptions = {
-    format: 'CODE128',
+    format: "CODE128",
     width: 2,
     height: 100,
     displayValue: true,
     text: undefined,
-    fontOptions: '',
-    font: 'monospace',
-    textAlign: 'center',
-    textPosition: 'bottom',
+    fontOptions: "",
+    font: "monospace",
+    textAlign: "center",
+    textPosition: "bottom",
     textMargin: 2,
     fontSize: 20,
-    background: '#ffffff',
-    lineColor: '#000000',
+    background: "#ffffff",
+    lineColor: "#000000",
     margin: 10,
     marginTop: undefined,
     marginBottom: undefined,
@@ -30,19 +30,19 @@
 
   onMount(async () => {
     await tick();
-    JsBarcode(barcode, value, optionsOA);
+    JsBarcode(barcode, value, defaultOptions);
   });
 
   afterUpdate(async () => {
     await tick();
-	  JsBarcode(barcode, value, Object.assign(defaultOptions, options));
+    JsBarcode(barcode, value, Object.assign(defaultOptions, options));
   });
 </script>
 
-{#if elementTag === 'img'}
+{#if elementTag === "img"}
   <img bind:this={barcode} alt="" />
-{:else if elementTag === 'canvas'}
-  <canvas bind:this={barcode}></canvas>
+{:else if elementTag === "canvas"}
+  <canvas bind:this={barcode} />
 {:else}
-  <svg bind:this={barcode}></svg>
+  <svg bind:this={barcode} />
 {/if}
